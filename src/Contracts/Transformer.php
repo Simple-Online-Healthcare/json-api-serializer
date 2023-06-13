8a74ec5b2b841055a593de720e1e4247b5e02237
Josh Murray <josh.murray@simpleonlinehealthcare.com>
2023-06-13 18:36:50 +0100

    Fixed the namespacing issue with Entity

--- src/Contracts/Transformer.php
+++ src/Contracts/Transformer.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace SimpleOnlineHealthcare\JsonApi\Contracts;
 
+use SimpleOnlineHealthcare\Contracts\Doctrine\Entity;
+
 interface Transformer
 {
     public function transform(Entity $entity): array;
