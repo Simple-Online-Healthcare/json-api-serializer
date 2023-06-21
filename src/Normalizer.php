0bcc953893e28be5b134bc01b320058bcf781204
Josh Murray <josh.murray@simpleonlinehealthcare.com>
2023-06-21 15:01:08 +0100

    removed the helper function

--- src/Normalizer.php
+++ src/Normalizer.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace SimpleOnlineHealthcare\JsonApi;
 
-use DateTimeInterface;
-use SimpleOnlineHealthcare\Contracts\Doctrine\Entity;
 use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 use Symfony\Component\Serializer\Normalizer\PropertyNormalizer;
@@ -31,14 +29,6 @@ abstract class Normalizer implements NormalizerInterface, DenormalizerInterface
         }
 
         return $data['type'] ?? null;
-    }
-
-    protected function timestamps(Entity $entity): array
-    {
-        return [
-            'createdAt' => $entity->getCreatedAt()->format(DateTimeInterface::ATOM),
-            'updatedAt' => $entity->getUpdatedAt()->format(DateTimeInterface::ATOM),
-        ];
     }
 
     protected function getPropertyNormalizer(): PropertyNormalizer
