b8e3104f6d0089a890debdf5b95e639deedd5876
Josh Murray <josh.murray@simpleonlinehealthcare.com>
2023-06-22 10:16:47 +0100

    working

--- src/Normalizers/EntityNormalizer.php
+++ src/Normalizers/EntityNormalizer.php
@@ -68,6 +68,21 @@ abstract class EntityNormalizer extends Normalizer
         return $this->getResourceTypeFromJson($data) === $this->resourceType;
     }
 
+    protected function getResourceTypeFromJson(array $data): ?string
+    {
+        // if the data key exists, it means the main request is going to fall in here
+        // which isn't correct. only the entity JSON should ever fall in here.
+        if (array_key_exists('data', $data)) {
+            return null;
+        }
+
+        if (array_key_exists('type', $data) === false) {
+            $data = reset($data);
+        }
+
+        return $data['type'] ?? null;
+    }
+
     protected function normalizeFields(array $fields): array
     {
         return array_map(function (mixed $field) {
--- src/Normalizers/Normalizer.php
+++ src/Normalizers/Normalizer.php
@@ -19,21 +19,6 @@ abstract class Normalizer implements NormalizerInterface, DenormalizerInterface
     {
     }
 
-    protected function getResourceTypeFromJson(array $data): ?string
-    {
-        // if the data key exists, it means the main request is going to fall in here
-        // which isn't correct. only the entity JSON should ever fall in here.
-        if (array_key_exists('data', $data)) {
-            return null;
-        }
-
-        if (array_key_exists('type', $data) === false) {
-            $data = reset($data);
-        }
-
-        return $data['type'] ?? null;
-    }
-
     protected function getPropertyNormalizer(): PropertyNormalizer
     {
         return $this->propertyNormalizer;
